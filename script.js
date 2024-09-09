6ba453d82f3dce737ce09bbb6a2c40ea2ae9cafd
Ifham-Hashir <ifham09052003@gmail.com>
2024-09-09 12:25:24 +0530

    Add Game Board in console

---
+++ script.js
@@ -0,0 +1,29 @@
+const gameBoard = (() => {
+  const board = [];
+  for (let i = 0; i < 3; i++) {
+    board[i] = [];
+    for (let j = 0; j < 3; j++) {
+      board[i].push(j);
+    }
+  }
+  return {board};
+}) ();
+
+console.log(gameBoard);
+
+// function createPlayers (playerOne, playerTwo){
+//   const players = [
+//     {
+//       playerName: playerOne,
+//       token: "X",
+//     },
+//     {
+//       playerName: playerTwo,
+//       token: "O",
+//     }
+//   ];
+
+//   return {players};
+// }
+
+// console.log(createPlayers("Ifham", "Metal"));
