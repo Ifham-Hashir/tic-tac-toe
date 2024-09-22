351c6f99c95abff4b31eb6db947a59539ba6f793
Ifham-Hashir <ifham09052003@gmail.com>
2024-09-22 08:08:20 +0530

    Display gameboard array

--- script.js
+++ script.js
@@ -101,4 +101,52 @@ function gameController() {
   }
 };
 
-gameController();
+function screenController() {
+  //const game = gameController();
+  const playerTurnDiv = document.querySelector('.turn');
+  const boardDiv = document.querySelector('.board');
+
+  const updateScreen = () => {
+    // clear the board
+    boardDiv.textContent = "";
+
+    // get the newest version of the board and player turn
+    const board = gameBoard.board;
+    //const activePlayer = game.getActivePlayer();
+
+    // Display player's turn
+    //playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+
+    // Render board squares
+    
+      board.forEach((cell, index) => {
+        // Anything clickable should be a button!!
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        // Create a data attribute to identify the column
+        // This makes it easier to pass into our `playRound` function 
+        cellButton.dataset.column = index;
+        cellButton.textContent = cell;
+        boardDiv.appendChild(cellButton);
+      })
+  }
+  updateScreen();
+}
+//   // Add event listener for the board
+//   function clickHandlerBoard(e) {
+//     const selectedColumn = e.target.dataset.column;
+//     // Make sure I've clicked a column and not the gaps in between
+//     if (!selectedColumn) return;
+    
+//     game.playRound(selectedColumn);
+//     updateScreen();
+//   }
+//   boardDiv.addEventListener("click", clickHandlerBoard);
+
+//   // Initial render
+//   updateScreen();
+
+//   // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+// }
+
+screenController();
